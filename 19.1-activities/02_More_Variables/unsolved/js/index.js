934f641b2dbde02055be94cdcd99662d6a7a27d8
rhondaeltobgi <111027319+rhondaeltobgi@users.noreply.github.com>
2023-01-12 21:47:41 -0500

    class

---
+++ 19.1-activities/02_More_Variables/unsolved/js/index.js
@@ -0,0 +1,10 @@
+// Create the addNumbers function here.
+function addNumbers() {
+    var numberTwo='2';
+    var numberThree='3';
+    var numberFive=2+3;
+   
+}
+console.log(numberFive);
+
+// Create the stringConcat function here.
